b3d12b15a87d823506aeb7dcabd96b7f7b8ae1d9
lavet13 <lavet13@mail.ru>
2023-05-01 19:31:01 +0300

    oops

--- src/components/product-card/product-card.styles.jsx
+++ src/components/product-card/product-card.styles.jsx
@@ -11,22 +11,6 @@ export const ProductCardContainer = styled.div`
   padding-bottom: 20px;
 `;
 
-export const ProductCardImageContainer = styled.div`
-  display: block;
-  width: 100%;
-  max-height: 150px;
-  overflow: hidden;
-`;
-
-export const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: auto;
-  object-fit: cover;
-  border-top-left-radius: 10px;
-  border-top-right-radius: 10px;
-`;
-
 export const ProductCardBottom = styled.div`
   display: flex;
   flex-direction: column;
